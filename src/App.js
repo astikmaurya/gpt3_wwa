2460ae2a9dbb8f5386ad03d78e7d67b0c5d393c3
astikmaurya <astikmaurya83@gmail.com>
2024-02-17 14:38:47 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Article from "./components/article/article.jsx";
+
+function App() {
+  return (
+    <div>
+      <App />
+      <Article />
+    </div>
+  );
+}
+
+export default App;
