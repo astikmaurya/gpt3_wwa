0c2d78a8c6ec960e96c2c265d0a015996b402cc0
astikmaurya <astikmaurya83@gmail.com>
2024-03-01 23:01:40 +0530

    building whatgpt3 section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./whatgpt3.css";
+import { Feature } from "../../components";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The Possibilities are beyond your imagination
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
